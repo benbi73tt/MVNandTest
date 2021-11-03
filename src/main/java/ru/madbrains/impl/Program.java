6182bb469c9503bd5af9038af70d3c6f3639238f
Anton <56966986+benbi73tt@users.noreply.github.com>
2021-11-03 13:38:21 +0400

    Initial commit

---
+++ src/main/java/ru/madbrains/impl/Program.java
@@ -0,0 +1,12 @@
+package ru.madbrains.impl;
+
+import ru.madbrains.simpleList.ListOperation;
+import ru.madbrains.simpleList.SimpleList;
+
+public class Program {
+    public static void main(String[] args){
+        System.out.println("hello");
+        SimpleList car = new ListOperation();
+
+    }
+}
